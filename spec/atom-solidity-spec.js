abc5ae8d15d13a85356796e49d6c4be92589f684
0mkara <0mkar@protonmail.com>
2017-03-09 18:22:35 +0530

    Compile on save

--- spec/atom-solidity-spec.js
+++ spec/atom-solidity-spec.js
@@ -6,62 +6,39 @@ import Etheratom from '../lib/ethereum-interface';
 // To run a specific `it` or `describe` block add an `f` to the front (e.g. `fit`
 // or `fdescribe`). Remove the `f` to unfocus the block.
 
-describe("Etheratom", function() {
-	let [workspaceElement, activationPromise] = Array.from([]);
+describe("Etheratom", () => {
+	let workspaceElement = null
+	let activationPromise = null
 
 	beforeEach(function() {
 		workspaceElement = atom.views.getView(atom.workspace);
-		return activationPromise = atom.packages.activatePackage('etheratom');
+		activationPromise = atom.packages.activatePackage('etheratom');
 	});
 
-	return describe("when the etheratom:toggle event is triggered", function() {
-		it("hides and shows the modal panel", function() {
-			// Before the activation event the view is not on the DOM, and no panel
-			// has been created
-			expect(workspaceElement.querySelector('.etheratom')).not.toExist();
+	describe("when the eth-interface:toggle event is triggered", () => {
+		it("hides and shows the modal panel", () => {
+			runs(() => {
+				// Before the activation event the view is not on the DOM, and no panel
+				// has been created
+				expect(workspaceElement.querySelector('.etheratom-panel')).not.toExist();
+			})
 
-			// This is an activation event, triggering it will cause the package to be
-			// activated.
-			atom.commands.dispatch(workspaceElement, 'etheratom:toggle');
-
-			waitsForPromise(() => activationPromise);
-
-			return runs(function() {
-				expect(workspaceElement.querySelector('.etheratom')).toExist();
-
-				let atomSolidityElement = workspaceElement.querySelector('.etheratom');
+			runs(() => {
+				// This is an activation event, triggering it will cause the package to be activated.
+				expect(workspaceElement.querySelector('.etheratom-panel')).not.toExist();
+				atom.commands.dispatch(workspaceElement, 'eth-interface:toggle');
+				let atomSolidityElement = workspaceElement.querySelector('.etheratom-panel');
 				expect(atomSolidityElement).toExist();
 
+				// This is a deactivation event, triggering it will cause the package to be deactivated.
 				let atomSolidityPanel = atom.workspace.panelForItem(atomSolidityElement);
 				expect(atomSolidityPanel.isVisible()).toBe(true);
-				atom.commands.dispatch(workspaceElement, 'etheratom:toggle');
-				return expect(atomSolidityPanel.isVisible()).toBe(false);
+				atom.commands.dispatch(workspaceElement, 'eth-interface:toggle');
+				expect(atomSolidityPanel.isVisible()).toBe(false);
 			});
-		});
 
-		return it("hides and shows the view", function() {
-			// This test shows you an integration test testing at the view level.
-
-			// Attaching the workspaceElement to the DOM is required to allow the
-			// `toBeVisible()` matchers to work. Anything testing visibility or focus
-			// requires that the workspaceElement is on the DOM. Tests that attach the
-			// workspaceElement to the DOM are generally slower than those off DOM.
-			jasmine.attachToDOM(workspaceElement);
-
-			expect(workspaceElement.querySelector('.etheratom')).not.toExist();
-
-			// This is an activation event, triggering it causes the package to be
-			// activated.
-			atom.commands.dispatch(workspaceElement, 'etheratom:toggle');
-
-			waitsForPromise(() => activationPromise);
-
-			return runs(function() {
-				// Now we can test for view visibility
-				let atomSolidityElement = workspaceElement.querySelector('.etheratom');
-				expect(atomSolidityElement).toBeVisible();
-				atom.commands.dispatch(workspaceElement, 'etheratom:toggle');
-				return expect(atomSolidityElement).not.toBeVisible();
+			waitsForPromise(() => {
+				return activationPromise;
 			});
 		});
 	});
