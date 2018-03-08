ee79df0bd028c3ce2bebae31e822d6e1c628f230
0mkar <0mkar@protonmail.com>
2018-03-08 19:41:06 +0530

    try to handle github/swarm/ipfs imports

--- spec/atom-solidity-spec.js
+++ spec/atom-solidity-spec.js
@@ -1,5 +1,6 @@
 'use babel'
-import Etheratom from '../lib/ethereum-interface';
+import Etheratom from '../lib/ethereum-interface'
+import { getHandlers, handleIPFS, resolveImports, combineSource } from '../lib/helpers/compiler-imports'
 
 // Use the command `window:run-package-specs` (cmd-alt-ctrl-p) to run specs.
 //
@@ -56,6 +57,40 @@ describe("Etheratom", async function() {
 			await atom.commands.dispatch(workspaceElement, 'eth-interface:toggle');
 			await atom.commands.dispatch(workspaceElement, 'eth-interface:toggle');
 			expect(workspaceElement.querySelector('.etheratom-panel')).not.toShow();
+		});
+	});
+
+	describe("Test compiler-imports functions", function() {
+		/*it("Expect handleIPFS to be a function", async function() {
+			console.log(await handleIPFS());
+			expect(typeof handleIPFS).toBe("function");
+		});
+		it("Expect handler() to return array", async function() {
+			const handlers = await getHandlers();
+			console.log(handlers);
+			expect(typeof handlers).toBe("object");
+		});
+		it("Expect resolveImports to resolve imports", async function() {
+			const subSource = await resolveImports('../', 'https://github.com/OpenZeppelin/zeppelin-solidity/contracts/crowdsale/Crowdsale.sol');
+			console.log(subSource);
+			console.log(typeof subSource);
+			expect(typeof subSource).toBe("string");
+		});*/
+		it("Expect combineSource to put all sources in one file", async function() {
+			const contract = `
+				/* Hello world contract */
+				pragma solidity ^0.4.18;
+				import './greeter.sol';
+				import 'https://github.com/OpenZeppelin/zeppelin-solidity/contracts/crowdsale/Crowdsale.sol';
+
+				contract Greetings is Greeter {
+				}
+			`
+			console.log(contract);
+			const dir = "/home/0mkar/Karma/Solidity";
+			const source = await combineSource(dir, contract, {});
+			console.log(source);
+			expect(typeof source).toBe("string");
 		});
 	});
 });
