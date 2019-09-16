dd103118bf5e11ffe9a30f8af832543e006c324a
avirupc817@gmail.com <avirupc817@gmail.com>
2019-09-16 13:50:44 +0530

    fixing travix error

---
+++ spec/.eslintrc.js
@@ -0,0 +1,50 @@
+module.exports = {
+    root: true,
+    parser: 'babel-eslint',
+    parserOptions: {
+      sourceType: 'module'
+    },
+    "env": {
+        "es6": true,
+        "node": true
+    },
+    "extends": ["standard", "eslint:recommended", "plugin:react/recommended"],
+    globals: {
+      __static: true,
+      atom: true
+    },
+    "parserOptions": {
+        "ecmaVersion": 6,
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "sourceType": "module"
+    },
+    "settings": {
+        "react": {
+            "version": "16.4.2"
+        }
+    },
+    "plugins": [
+        "react",
+        "babel"
+    ],
+    "rules": {
+        "strict": 0,
+        "babel/semi": 1,
+        // tab indentation
+        'indent': ["warn", 4],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        // allow paren-less arrow functions
+        'arrow-parens': 0,
+        'space-before-function-paren': ["error", "never"],
+        'no-console': ["error", { allow: ["warn", "error", "log"] }]
+    }
+};
